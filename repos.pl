016b952b501608241cb80a87b8dadba9e1d7ea4d
Matteo Pasotti <matteo@xquiet.eu>
2012-09-14 00:48:49 +0200

    - added basic support to addmedia and removemedia

--- repos.pl
+++ repos.pl
@@ -22,6 +22,11 @@ use Term::ANSIColor qw(:constants);
 
 my $PKG_QUERYMAKER = "urpmq";
 my $QUERYMAKER_PARAM = "--list-media";
+my $REPO_ADDMEDIA = "urpmi.addmedia";
+my $REPO_ADDMEDIA_PARAM_DISTRIB = "--distrib";
+my $REPO_ADDMEDIA_PARAM_MIRRORLIST = "--mirrorlist";
+my $REPO_RMMEDIA = "urpmi.removemedia";
+my $REPO_RMMEDIA_ALL = "-a";
 my $REPO_ENABLER = "urpmi.update";
 my $REPO_PARAM_ACTIVATE = "--no-ignore";
 my $REPO_PARAM_DEACTIVATE = "--ignore";
@@ -74,8 +79,14 @@ sub main {
 	}
 	close(HFILE);
 	if(scalar(@list) le 0){
-		print "Nessun media presente.\nEsegui urpmi.addmedia --distrib --mirrorlist\n";
-		return 0;
+		print "No available medias.\n Do I have to add all the default medias? (Y/n)\n";
+		$input=<STDIN>;
+		chomp $input;
+		if (lc($input) eq "y"){
+			return add_medias();
+		}else{
+			return 0;
+		}
 	}
 	# active medias
 	open(HFILE, $PKG_QUERYMAKER." ".$QUERYMAKER_PARAM." active |") || die("Can't open stream\n");
@@ -117,9 +128,14 @@ sub main {
 	chomp $input;
 	return 1 if ($input eq "");
 	return 0 if ($input eq "q");
+	return add_medias() if ($input eq "amdm");
+	return remove_medias() if ($input eq "rmall");
 	return update_repos() if ($input eq "u");
 	return show_help() if ($input eq "h");
-	
+
+	# out of range
+	return 1 if (($input-1) > scalar(grep $_, @list));
+
 	$active=1 if(grep {$_ eq $list[$input-1]} @actives);
 	$active=0 if(!grep {$_ eq $list[$input-1]} @actives);
 	toggle($list[$input-1],$active) if(exists($list[$input-1]));
@@ -164,13 +180,40 @@ sub update_repo {
 	return system(@args);
 }
 
+sub add_medias {
+	my @args = ();
+	push(@args, "/usr/bin/env");
+	push(@args, $REPO_ADDMEDIA);
+	push(@args, $REPO_ADDMEDIA_PARAM_DISTRIB);
+	push(@args, $REPO_ADDMEDIA_PARAM_MIRRORLIST);
+	print "@args\n";
+	system(@args);
+	return 1;
+}
+
+sub remove_medias {
+	my @args = ();
+	print "Are you sure you want to remove ALL medias? Digit YES to confirm, just [Enter] to go back\n";
+	my $input = <STDIN>;
+	chomp $input;
+	return 1 if (uc($input) ne "YES");
+	push(@args, "/usr/bin/env");
+	push(@args, $REPO_RMMEDIA);
+	push(@args, $REPO_RMMEDIA_ALL);
+	system(@args);
+	return 1;
+}
+
 sub show_help {
 	system('clear');
 	print BRIGHT_BLUE, "Digit the media correspondent number then press ",BOLD,"[Enter]",RESET,BRIGHT_BLUE," to toggle the repo active/inactive\n", RESET;
 	print "Press ", BOLD, "q", RESET, " then [Enter] to quit\n";
 	print "Press ", BOLD, "u", RESET, " then [Enter] to refresh medias\n";
 	print "Press ", BOLD, "h", RESET, " then [Enter] for this help\n";
+	print "Press ", BOLD, "amdm", RESET, " then [Enter] to add default medias\n";
+	print "Press ", BOLD, "rmall", RESET, " then [Enter] to remove all available medias\n";
 	print "Press ", BOLD, "[Enter]", RESET, " to quit this help and go back to the repositories\n";
+	print ": ";
 	<STDIN>;
 	return 1;
 }
