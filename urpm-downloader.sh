66563b4804a5e8032695725373a95ab42e0e382a
Matteo Pasotti <matteo@xquiet.eu>
2012-10-16 02:07:10 +0200

    improved script description into the header

--- urpm-downloader.sh
+++ urpm-downloader.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # @copyright 2012 by Matteo Pasotti <matteo.pasotti@gmail.com>
+# script inspired by yum-downloader
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
