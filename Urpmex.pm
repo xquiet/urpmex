71205229406d3eec5658b46a1a0cb1372b50a4b9
Matteo Pasotti <matteo@xquiet.eu>
2012-12-08 11:07:34 +0100

    added retrieve_available_updates added wrap_retrieve_installed_packages added retrieve_installed_packages added wrap_retrieve_available_packages added retrieve_available_packages_release added retrieve_available_packages_full renamed enumerate_symm_diff added enumerate_union added enumerate_intersection updated export

--- Urpmex.pm
+++ Urpmex.pm
@@ -26,7 +26,10 @@ use Data::Dumper;
 use List::Compare;
 use List::Util qw(first);
 
-our @EXPORT = qw(retrieve_brpm_pkgname
+our @EXPORT = qw(retrieve_available_updates
+             retrieve_available_packages_full
+	     retrieve_available_packages_release
+	     retrieve_brpm_pkgname
              retrieve_srpm_pkgname
 	     retrieve_active_media_urls
 	     retrieve_medias_array 
@@ -39,14 +42,24 @@ our @EXPORT = qw(retrieve_brpm_pkgname
              remove_medias 
              update_repos
              compute_changes
-             enumerate_unselected_repos);
+             enumerate_symm_diff
+	     $WITH_GROUP
+	     $WITHOUT_GROUP
+	     $UPDATES_ONLY
+     	     );
 
 my $PKG_QUERYMAKER = "urpmq";
+my $RPM_COMMAND = "rpm";
+my $RPM_QUERY = "-q";
+my $RPM_QUERY_ALL = "-a";
+my $RPM_QUERY_FMT = "--qf";
 my $QUERY_LIST_AVAILABLE_PACKAGES = "--list";
 my $QUERY_LISTMEDIA_PARM = "--list-media";
 my $QUERY_LISTURL_PARM = "--list-url";
+my $QUERY_LIST_UPDATES_ONLY = "--update";
 my $QUERY_LOOKFORSRPM_PARM = "--sourcerpm";
 my $QUERY_PKG_FULL = "-f";
+my $QUERY_PKG_RELEASE = "-r";
 my $QUERY_PKG_GROUP = "-g";
 my $DLDER = "--wget";
 
@@ -59,13 +72,100 @@ my $REPO_ENABLER = "urpmi.update";
 my $REPO_PARAM_ACTIVATE = "--no-ignore";
 my $REPO_PARAM_DEACTIVATE = "--ignore";
 
+# constants
+our $WITH_GROUP = 1;
+our $WITHOUT_GROUP = 0;
+our $UPDATES_ONLY = 1;
+our $RPM_QUERY_NAMEONLY = 1;
+
+# ----------------------------------------------------------------------
+# find available updates - array
+# ----------------------------------------------------------------------
+sub retrieve_available_updates {
+	my @installed = retrieve_installed_packages($RPM_QUERY_NAMEONLY);
+	my @availables = retrieve_available_packages_release($WITHOUT_GROUP,$UPDATES_ONLY);
+	my @intermediate = enumerate_symm_diff(\@installed, \@availables);
+	print "INTERMEDIATE\n";
+	print Dumper(@intermediate);
+	print "INSTALLED\n";
+	print Dumper(@installed);
+	my @updates = enumerate_intersection(\@installed, \@intermediate);
+	return @updates;
+}
+
+sub wrap_retrieve_installed_packages {
+	my $nameonly = shift();
+	my @args;
+	my $command = "$RPM_COMMAND $RPM_QUERY $RPM_QUERY_ALL";
+	if(defined($nameonly) && ($nameonly == $RPM_QUERY_NAMEONLY)){
+		push @args, "$RPM_QUERY_FMT \"%{NAME}\n\"";
+	}
+	$command = $command . " " . join(" ", @args);
+	return $command;
+}
+
+# ----------------------------------------------------------------------
+# retrieve installed packages - array
+# ----------------------------------------------------------------------
+sub retrieve_installed_packages {
+	my $nameonly = shift();
+	my $command = wrap_retrieve_installed_packages($nameonly);
+	my @list = `$command`;
+	chomp @list;
+	return @list;
+}
+
+# ----------------------------------------------------------------------
+# wrap command retrieve_available_packages
+# @parm full   bool   if you want full output from urpmq
+# @parm group  bool   if you want group output from urpmq
+# @parm update bool   if you want to list updates only
+# ----------------------------------------------------------------------
+sub wrap_retrieve_available_packages {
+	my $full = shift();
+	my $group = shift();
+	my $update = shift();
+	my @args;
+	my $command = $PKG_QUERYMAKER;
+	push @args, $QUERY_LIST_AVAILABLE_PACKAGES;
+	if(defined($update) && ($update == $UPDATES_ONLY)){
+		push @args, $QUERY_LIST_UPDATES_ONLY;
+	}
+	if(defined($group) && ($group == $WITH_GROUP)){
+		push @args, $QUERY_PKG_GROUP;
+		print "Added -g\n";
+	}
+	if($full){
+		push @args, $QUERY_PKG_FULL;
+	}else{
+		push @args, $QUERY_PKG_RELEASE;
+	}
+	$command = $command ." ". join(" ",@args) . "| sort -u";
+	return $command;
+}
 
 # ----------------------------------------------------------------------
 # retrieve the list of the available packages - array
+# NOTE: no arch specified!
 # ----------------------------------------------------------------------
-sub retrieve_available_packages {
-	my $filter_arch = shift();
-	my @list_pkgs = `$PKG_QUERYMAKER $QUERY_LIST_AVAILABLE_PACKAGES $QUERY_PKG_GROUP $QUERY_PKG_FULL | sort -u`;
+sub retrieve_available_packages_release {
+	my $group = shift();
+	my $update = shift();
+	my $command = wrap_retrieve_available_packages(0,$group,$update);
+	my @list_pkgs = `$command`;
+	chomp @list_pkgs;
+	return @list_pkgs;
+}
+
+# ----------------------------------------------------------------------
+# retrieve the list of the available packages - array
+# NOTE: with arch specified
+# ----------------------------------------------------------------------
+sub retrieve_available_packages_full {
+	my $group = shift();
+	my $update = shift();
+	my $command = wrap_retrieve_available_packages(1,$group,$update);
+	my @list_pkgs = `$command`;
 	chomp @list_pkgs;
 	return @list_pkgs;
 }
@@ -268,7 +368,7 @@ sub compute_changes {
 	my $inactiverepos = shift();
 	my @ids_of_inactive_repos = sort @$inactiverepos;
 	# current unselected repos
-	my @currentlyUnselected = enumerate_unselected_repos(\@currSelection, \@allRepos);
+	my @currentlyUnselected = enumerate_symm_diff(\@currSelection, \@allRepos);
 
 	my @MEDIASTOENABLE;
 	my @MEDIASTODISABLE;
@@ -302,7 +402,7 @@ sub compute_changes {
 # ----------------------------------------------------------------------
 # returns an array of unselected repos from the list
 # ----------------------------------------------------------------------
-sub enumerate_unselected_repos {
+sub enumerate_symm_diff {
 	# ottengo i repository non abilitati/non selezionati
 	my $selection = shift();
 	my $repository = shift();
@@ -313,5 +413,32 @@ sub enumerate_unselected_repos {
 	return @enumerated_repos;
 }
 
+# ----------------------------------------------------------------------
+# returns the union of two lists - array
+# ----------------------------------------------------------------------
+sub enumerate_union {
+	my $first_set = shift();
+	my $second_set = shift();
+	my $union = List::Compare->new('--unsorted',\@$first_set,\@$second_set);
+	my @enumeration = $union->get_union();
+	return @enumeration;
+}
+
+# ----------------------------------------------------------------------
+# returns the intersection of two lists - array
+# ----------------------------------------------------------------------
+sub enumerate_intersection {
+	my $first_set = shift();
+	my $second_set = shift();
+=comment
+	my $intersection = List::Compare->new('--unsorted',\@$first_set,\@$second_set);
+	my @enumeration = $intersection->get_intersection();
+=cut
+	my @enumeration;
+	for my $pattern(@$first_set){
+		push @enumeration, grep { $_ =~ /${pattern}\-/g } @$second_set;
+	}
+	return @enumeration;
+}
 
 1;
