80171e5782f8f8297920661146d74a870547096f
xquiet <matteo@mageia.it>
2013-12-29 02:17:53 +0100

    - add const for pkg installer and deps resolution - use English - use Modern::Perl - basic sub check_privileges implemented

--- urpmex/Shared.pm
+++ urpmex/Shared.pm
@@ -3,7 +3,11 @@ package urpmex::Shared;
 require Exporter;
 use base qw(Exporter);
 
+use Modern::Perl;
+use English qw(-no_match_vars);
+
 our @EXPORT = qw($PKG_QUERYMAKER
+		$PKG_INSTALLER
 		$RPM_COMMAND
 		$RPM_VERCMP
 		$RPM_QUERY
@@ -18,6 +22,11 @@ our @EXPORT = qw($PKG_QUERYMAKER
 		$QUERY_PKG_RELEASE
 		$QUERY_PKG_GROUP
 		$QUERY_PKG_INFORMATIONS
+		$QUERY_PKG_DEPENDENCIES
+		$INSTALLER_FORCE
+		$INSTALLER_AUTO
+		$INSTALLER_AUTORESOLVEDEPS
+		$INSTALLER_MANUALLYRESOLVEDEPS
 		$DLDER
 		$REPO_ADDMEDIA
 		$REPO_ADDMEDIA_PARAM_DISTRIB
@@ -33,10 +42,12 @@ our @EXPORT = qw($PKG_QUERYMAKER
 		$RPM_QUERY_NAMEONLY
 		$RPM_QUERY_NAME_VERSION_RELEASE
 		indexArray
+		check_privileges
 		trim);
 
 
 our $PKG_QUERYMAKER = "urpmq";
+our $PKG_INSTALLER = "urpmi";
 our $RPM_COMMAND = "rpm";
 our $RPM_QUERY = "-q";
 our $RPM_QUERY_ALL = "-a";
@@ -46,6 +57,11 @@ our $QUERY_LISTMEDIA_PARM = "--list-media";
 our $QUERY_LISTURL_PARM = "--list-url";
 our $QUERY_LIST_UPDATES_ONLY = "--update";
 our $QUERY_LOOKFORSRPM_PARM = "--sourcerpm";
+# QUERY_PKG_DEPENDENCIES -d (dependencies) and -m for display what's not already installed
+# man urpmq
+# -m equivalent to -d -u or -du
+# http://wiki.rosalab.ru/en/index.php/Urpmi#Getting_a_list_of_dependencies_before_installing
+our $QUERY_PKG_DEPENDENCIES = "-m";
 our $QUERY_PKG_FULL = "-f";
 our $QUERY_PKG_RELEASE = "-r";
 our $QUERY_PKG_GROUP = "-g";
@@ -63,6 +79,11 @@ our $REPO_ENABLER = "urpmi.update";
 our $REPO_PARAM_ACTIVATE = "--no-ignore";
 our $REPO_PARAM_DEACTIVATE = "--ignore";
 
+our $INSTALLER_FORCE = "--force";
+our $INSTALLER_AUTO = "--auto";
+our $INSTALLER_AUTORESOLVEDEPS = 'A';
+our $INSTALLER_MANUALLYRESOLVEDEPS = 'M';
+
 # constants
 our $WITH_GROUP = 1;
 our $WITHOUT_GROUP = 0;
@@ -74,6 +95,16 @@ our $RPM_QUERY_NAME_VERSION_RELEASE = 2;
 sub indexArray{
 	1 while $_[0] ne pop;
 	@_-1;
+}
+
+######################################################
+# check_privileges - bool
+# @desc if the script is running as root, return true
+#       otherwise return false
+######################################################
+
+sub check_privileges {
+	return $EUID == 0;
 }
 
 sub trim {
